317dcb5d84411f47d67ad274f73000691924177d
rhsabbir1 <rhsabbir090@gmai.com>
2023-02-16 15:41:05 +0600

    price update usuing function

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 // for first card 
-let count = 0;
+// let count = 0;
+
+function showData (productName,productPrice,procuctQuantity,totalPRice){
+  const tableContainer = document.getElementById('table-container')
+  const tr = document.createElement('tr');
+  tr.innerHTML = `<th>${1}</th>
+  <td>${productName}</td>
+  <td>${productPrice}</td>
+  <td>${procuctQuantity}</td>
+  <td>${totalPRice}</td>`
+
+  tableContainer.appendChild(tr)
+}
+
+
 document.getElementById('first-card').addEventListener('click', function () {
   const productName = document.getElementById('first-name').innerText;
 
@@ -7,20 +21,23 @@ document.getElementById('first-card').addEventListener('click', function () {
 
   const procuctQuantity = document.getElementById('first-quantity').innerText;
 
-  const totalPRice = productPrice * procuctQuantity;
-  console.log(productName, productPrice, procuctQuantity, totalPRice)
+  const totalPRice = parseInt(productPrice) * parseInt(procuctQuantity);
+  // console.log(productName, productPrice, procuctQuantity, totalPRice)
 
-  count = count + 1;
+  // count = count + 1;
 
-  const tableContainer = document.getElementById('table-container')
-  const tr = document.createElement('tr');
-  tr.innerHTML = `<th>${count}</th>
+  showData(productName,productPrice, procuctQuantity,totalPRice)
+  // const tableContainer = document.getElementById('table-container')
+  // const tr = document.createElement('tr');
+  // tr.innerHTML = `<th>${count}</th>
  
-  <td>${productName}</td>
-  <td>${productPrice}</td>
-  <td>${procuctQuantity}</td>
-  <td>${totalPRice}</td>`
+  // <td>${productName}</td>
+  // <td>${productPrice}</td>
+  // <td>${procuctQuantity}</td>
+  // <td>${totalPRice}</td>`
 
-  tableContainer.appendChild(tr)
+  // tableContainer.appendChild(tr)
 
 })
+
+
